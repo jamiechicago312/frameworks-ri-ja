7fea2352063a2f377df8415c4efe6b3ffd53693b
Jamie Chicago <87397251+jamiechicago312@users.noreply.github.com>
2024-03-22 12:32:00 -0500

    add frame depencies

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,15 @@
+import { fetchMetadata } from "frames.js/next";
+
+export async function generateMetadata() {
+  return {
+    title: "My Page",
+    // provide a full URL to your /frames endpoint
+    ...(await fetchMetadata(
+      new URL("/frames", process.env.VERCEL_URL || "http://localhost:3000")
+    )),
+  };
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
