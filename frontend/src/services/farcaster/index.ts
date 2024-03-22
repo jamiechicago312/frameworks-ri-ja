cf7b056da7f1140d90c459ff75c3c198022d7e2f
Jamie Chicago <87397251+jamiechicago312@users.noreply.github.com>
2024-03-22 18:33:26 -0500

    Add fetch cast function; create cast image
    Add fetch cast from wield API
    Starting code for cast image

--- frontend/src/services/farcaster/index.ts
+++ frontend/src/services/farcaster/index.ts
@@ -1,5 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+const KEY = Deno.env.get("YOUR_WEILD_KEY_ENV"); 
+// ^ where can we keep this save in this repo
 
-// need to install `npm install node-fetch@2 --save`
-// like URL sample: 
+
+
+export default async function fetchCast(shortHash: string, username: string) {
+  try {
+    const res = await fetch(
+      `https://protocol.wield.co/farcaster/v2/cast-short?shortHash=${shortHash}&username=${username}`, // Fixed URL syntax
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "API-KEY": KEY!,
+        },
+      },
+    );
+    const resJson = await res.json();
+    console.log(resJson.result.cast);
+    return resJson.result.cast;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+//code to look up cast in API
+export const imgUrl = async (request) => {
+
+  app.post("/mint", async () => { //don't know if it should say /mint
+    try {
+      const searchParams = new URL(request.url).searchParams;
+      const castUrl = searchParams.get("cast") //castUrl will be user input
+      if (!castUrl.startsWith("https://warpcast.com")) {
+        return {
+          status: 400,
+          body: "Invalid cast URL. URL must start with 'https://warpcast.com'."
+        };
+      }
+      const address = searchParams.get("address")
+      const parts = castUrl.split('/');
+      const username = parts[3]; // takes username out
+      const hashShort = parts[4]; //takes short hash out
+      const rawCast = await fetchCast(hashShort, username) //use wield API
+      const hashLong: string = resJson.result.cast.hash; //pulls out the full hash
+      const saveImage = new URL(`https://client.warpcast.com/v2/cast-image?castHash=${hashLong}`) 
+ }
